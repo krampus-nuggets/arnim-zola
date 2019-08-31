917f9f68be0fad7c638e892e3b352a74017b8fd7
Krampus Nuggets <sp3cter.productions@wemakeart.co.za>
2019-08-31 16:08:14 +0200

    UPDATE => Define getresponse Function

--- src/modules/findAdmin.py
+++ src/modules/findAdmin.py
@@ -55,3 +55,45 @@ class Printer:
     def __init__(self, data):
         stdout.write("\r\x1b[K" + data.__str__())
         stdout.flush()
+
+def getresponse(threadName, q):
+    while not exitFlag:
+        queueLock.acquire()
+        if not workQueue.empty():
+            data = q.get()
+            queueLock.release()
+            checkg = 1
+            while checkg == 1:
+                try:
+                    connection = http.client.HTTPConnection(str(url))
+                    connection.request('HEAD', "/" + str(data.strip()))
+                    response = connection.getresponse()
+                    progdone = len(adminlist) - workQueue.qsize()
+                    update = " [>] Checking " + str(progdone) + "/" + str(len(adminlist)) + " " + str(url) + "/" + str(data.strip()) + " \t[" + str(response.status) + "]"
+                    Printer(update)
+                    checkg += 1
+                    reporturl = "\r\x1b[K [*] " + str(url) + "/" + str(data.strip())
+                    if len(reporturl) < 60:
+                        add = 60 - int(len(reporturl))
+                        reporturl = str(reporturl) + str(" ") * int(add)
+                        reportcode = "[" + str(response.status) + "]"
+                    if response.status == 200:
+                        print(str(reporturl) + str(reportcode) + " OK")
+                        found.append(response.status)
+                    elif 300 <= response.status < 400 and args.follow:
+                        reso = httplib2.Http(".cache_httplib")
+                        reso.follow_all_redirects = True
+                        link = "http://" + str(url) + "/" +str(data.strip())
+                        resp = reso.request(link, "HEAD")[0]
+                        finalurl = resp['content-location']
+                        if finalurl[0:5] == "http:":
+                            finalurl = finalurl[11:]
+                        elif finalurl[0:5] == "https":
+                            finalurl = " [HTTPS] " + finalurl[12:]
+                        print(str(reporturl) + str(reportcode) + " Redirect >> " + str(finalurl))
+                    elif response.status == 403 and args.forbidden:
+                        print(str(reporturl) + str(reportcode) + " Forbidden!")
+                except:
+                    pass
+        else:
+            queueLock.release()
