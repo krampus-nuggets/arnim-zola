be6663a4b8ac3b3fcff7fbdbb411da563f07cd14
Krampus Nuggets <sp3cter.productions@wemakeart.co.za>
2019-08-31 15:46:10 +0200

    UPDATE => Process Time Evaluation

--- src/modules/findAdmin.py
+++ src/modules/findAdmin.py
@@ -30,3 +30,23 @@ class myThread(Thread):
 
     def run(self):
         getresponse(self.name, self.q)
+
+class Timer:
+    def __enter__(self):
+        self.start = time.time()
+
+    def __exit__(self, *args):
+        taken = time.time() - self.start
+        seconds = int(time.strftime('%S', time.gmtime(taken)))
+        minutes = int(time.strftime('$M', time.gmtime(taken)))
+        hours = int(time.strftime('%H', time.gmtime(taken)))
+        if minutes > 0:
+            if hours > 0:
+                print(" [*] Time Elapsed " + str(hours) + " hours, " + str(minutes) + " minutes and " + str(seconds) + " seconds at " + str(round(len(adminlist) / taken, 2)) + " lookups per second.")
+            else:
+                print(" [*] Time Elapsed " + str(minutes) + " minutes and " + str(seconds) + " seconds at " + str(round(len(adminlist) / taken, 2)) + " lookups per second.")
+        else:
+            print(" [*] Time Elapsed " + str(seconds) + " seconds at " + str(round(len(adminlist) / taken, 2)) + " lookups per second.")
+        maked = "rm -rf .cache_httplib"
+        process = subprocess.Popen(maked.split(), stdout=subprocess.PIPE)
+        poutput = process.communicate()[0]
